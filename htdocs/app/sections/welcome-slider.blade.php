6757ae3a394c5aa20c33decfea421c03f0c75127
Daniel Raudschus <daniel.raudschus@a365.at>
2017-08-08 14:41:10 +0200

    adding plugins and creating first section (WIP)

--- htdocs/app/sections/welcome-slider.blade.php
+++ htdocs/app/sections/welcome-slider.blade.php
@@ -1,19 +1,18 @@
-<?php 
-
-	$slides = $block->getSlides();
-	// $pretitle = $block->getPretitle();
-	// $title = $block->getTitle();
-	// $description = $block->getDescription();
-	// $links = $block->getImages();
+<?php
+    $bgimg = $block->getBackground();
+    $firstline =  $block->getFirstline();
+    $secondline =  $block->getSecondline();
+    $thirdline =  $block->getThirdline();
+    $forthline =  $block->getForthline();
 ?>
 
-<div class="welcome-slider">
-	@foreach ($slides as $slide)
-		<div class="welcome-slider__slide">
-			<div class="welcome-slider__slide__image">
-				<img src="{{$slide['background']}}" alt="" />
+		<div class="welcome-slider">
+			<img class="welcome-slider__img" src="{{$bgimg}}" alt="Startseiten Bild">
+			<div class="welcome-slider__headline">
+				<div class="welcome-slider__headline headline--first">{{$firstline}}</div>
+				<div class="welcome-slider__headline headline--second">{{$secondline}}</div>
+				<div class="welcome-slider__headline headline--third">{{$thirdline}}</div>
+				<div class="welcome-slider__headline headline--forth">{{$forthline}}</div>
 			</div>
 		</div>
-	@endforeach
 
-</div>
