a4a9028f9f20da9ab338739e3210857c60b0146b
Daniel Raudschus <daniel.raudschus@a365.at>
2017-08-09 11:37:42 +0200

    changing different files by deleting committed code that is no longer be needed

--- htdocs/app/sections/welcome-slider.blade.php
+++ htdocs/app/sections/welcome-slider.blade.php
@@ -5,13 +5,15 @@
     $thirdline =  $block->getThirdline();
     $forthline =  $block->getForthline();
 ?>
-<div class="welcome-slider">
-	<img class="welcome-slider__img" src="{{$bgimg}}" alt="Startseiten Bild">
-	<div class="welcome-slider__text h1 section__content">
-		<div class="welcome-slider__text__first">{{$firstline}}</div>
-		<div class="welcome-slider__text__second">{{$secondline}}</div>
-		<div class="welcome-slider__text__third">{{$thirdline}}</div>
-		<div class="welcome-slider__text__forth">{{$forthline}}</div>
+<div class="section">
+	<div class="welcome-slider">
+		<img class="welcome-slider__img" src="{{$bgimg}}" alt="Startseiten Bild">
+		<div class="welcome-slider__text h1 section__content">
+			<div class="welcome-slider__text__first">{{$firstline}}</div>
+			<div class="welcome-slider__text__second">{{$secondline}}</div>
+			<div class="welcome-slider__text__third">{{$thirdline}}</div>
+			<div class="welcome-slider__text__forth">{{$forthline}}</div>
+		</div>
 	</div>
 </div>
 
