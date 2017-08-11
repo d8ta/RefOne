05eaa13f6a7344e64841434ccf4e4b2199142168
Daniel Raudschus <daniel.raudschus@a365.at>
2017-08-11 09:30:53 +0200

    intro section is with true false option about showing img, btn, grey background and white color -> radiobtn in backend

--- htdocs/app/classes/Project/Sections/Intro.php
+++ htdocs/app/classes/Project/Sections/Intro.php
@@ -16,10 +16,11 @@ class Intro extends \A365\Wordpress\Block\AcfBlock {
 		return [
 			FieldHelper::createTrueFalse('dark', 'Dunkler Hintergrund, helle Schrift'),
 			FieldHelper::createText('headline', 'Überschrift')->setRequired(),
+			FieldHelper::createText('subline', 'Unterüberschrift'),
 			FieldHelper::createWYSIWYG('text', 'Text')->setRequired(),
-			FieldHelper::createImageCrop('logos', 'Logos', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 1600, "height" => 650])->setRequired(),
+			FieldHelper::createImageCrop('logos', 'Logos', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 1600, "height" => 650]),
 			FieldHelper::createPageLink('btnurl', 'Button Link'),
-			FieldHelper::createText('btntext', 'Button Text')->setRequired(),
+			FieldHelper::createText('btntext', 'Button Text'),
 		];
 	}
 }
--- htdocs/app/sections/intro.blade.php
+++ htdocs/app/sections/intro.blade.php
@@ -1,5 +1,6 @@
 <?php
 	$headline = $block->getHeadline();
+	$subline = $block->getSubline();
 	$text = $block->getText();
     $logos = $block->getLogos();
     $btnurl = $block->getBtnurl();
@@ -9,11 +10,13 @@
 <div class="section__intro @if($dark) section__darkintro @endif">
 	<div class="section__content">	
 		<div class="intro">
-			<h2 class="intro__headline h1 @if($dark) intro__headline h1-dark @endif">{{$headline}}</h2>
+			<h2 class="intro__headline h1 @if($dark) intro__headline h1-dark @endif">{{$headline}} <span class="intro__subline h2">{{$subline}}</span></h2>
 			<div class="intro__text editor-content text-base @if($dark) intro__text editor-content text-base-dark @endif">{!!$text!!}</div>
 			<img src="{{$logos}}" alt="{{$logos}} Bild" class="intro__logos @if($dark) hidden @endif">
 			<div class="intro__button"></div>
-			<a href="{{$btnurl}}" class="btn @if($dark) btn--hidden @endif">{{$btntext}}</a>
+			<div class="@if($dark) hidden @endif">
+				<a href="{{$btnurl}}" class="btn">{{$btntext}}</a>
+			</div>
 		</div>
 	</div>
 </div>
