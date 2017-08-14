e4ea40758fe26561ceffc797ab382662a177abf1
Daniel Raudschus <daniel.raudschus@a365.at>
2017-08-14 15:39:47 +0200

    layout fab WIP

--- htdocs/app/sections/faq.blade.php
+++ htdocs/app/sections/faq.blade.php
@@ -2,14 +2,20 @@
     <div class="section__content">
         <div class="faq">
             <div class="faq__content">
-                <h2 class="h2">FAQ</h2>
+                <div class="faq__logobox">
+                    <h2 class="h1">FAQ</h2>
+                    <img class="faq__logo" src="" alt="logo"> 
+                </div>
                 <div class="faq js-accordion">
                     <div class="faq__item">
                         <div class="js-accordion__item">
                             <h3 class="h3">Gibt es auch fertige Plugins für Shopsysteme?</h3></div>
                         <div class="js-accordion__item__text">
                             <ol>
-                                <li></li>
+                                <li>test</li>
+                                <li>test</li>
+                                <li>test</li>
+                                <li>test</li>
                             </ol>
                         </div>
                     </div>
@@ -18,7 +24,10 @@
                             <h3 class="h3">Funktioniert die Anbindung mit unterschiedlichen Script Sprachen?</h3></div>
                         <div class="js-accordion__item__text">
                             <ol>
-                               <li></li>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>
                             </ol>
                         </div>
                     </div>
@@ -27,8 +36,10 @@
                             <h3 class="h3">Können auch wiederkehrende Zahlungen wie Abosabgewickelt werden?</h3></div>
                         <div class="js-accordion__item__text">
                             <ol>
-                                <li></li>
-                            </ol>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>                            </ol>
                         </div>
                     </div>
                     <div class="faq__item">
@@ -36,8 +47,10 @@
                             <h3 class="h3">Gibt es auch eine Server to Server Variante?</h3></div>
                         <div class="js-accordion__item__text">
                             <ol>
-                                <li></li>
-                            </ol>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>                            </ol>
                         </div>
                     </div>
                     <div class="faq__item">
@@ -45,8 +58,10 @@
                             <h3 class="h3">Kann ich die Farben und das Design anpassen?</h3></div>
                         <div class="js-accordion__item__text">
                             <ol>
-                                <li></li>
-                            </ol>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>                            </ol>
                         </div>
                     </div>
                     <div class="faq__item">
@@ -54,8 +69,10 @@
                             <h3 class="h3">Gibt es auch fertige Plugins für Shopsysteme?</h3></div>
                         <div class="js-accordion__item__text">
                             <ol>
-                                <li></li>
-                            </ol>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>                            </ol>
                         </div>
                     </div>
                     <div class="faq__item">
@@ -63,8 +80,10 @@
                             <h3 class="h3">Funktioniert die Anbindung mit unterschiedlichen Script Sprachen?</h3></div>
                         <div class="js-accordion__item__text">
                             <ol>
-                                <li></li>
-                            </ol>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>                            </ol>
                         </div>
                     </div> 
                     <div class="faq__item">
@@ -72,8 +91,10 @@
                             <h3 class="h3">Können auch wiederkehrende Zahlungen wie Abosabgewickelt werden?</h3></div>
                         <div class="js-accordion__item__text">
                             <ol>
-                                <li></li>
-                            </ol>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>
+                               <li>test</li>                            </ol>
                         </div>
                     </div>
                 </div>
