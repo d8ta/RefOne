961b006a2b1e07164cea4b831b6be1b374cfcf99
Daniel Raudschus <daniel.raudschus@a365.at>
2017-08-14 15:12:09 +0200

    responsive ready partner section

--- htdocs/app/classes/Project/Sections/Partner.php
+++ htdocs/app/classes/Project/Sections/Partner.php
@@ -14,7 +14,6 @@ class Partner extends \A365\Wordpress\Block\AcfBlock {
 	public static function getAcfSubfields()
 	{
 		return [
-			FieldHelper::createText('title', 'Sektionstitel')->setRequired(),
 			FieldHelper::createRepeater('partners', 'Partner', ["min" => 1, "max" => 24])
 				->addSubfields([
 					FieldHelper::createImageCrop('image', 'Hintergrund', ["crop_type" => "hard", 'target_size' => 'custom', 'force_crop' => 'no', 'save_format' => 'url', "width" => 150, "height" => 100])->setRequired(),
--- htdocs/app/sections/partner.blade.php
+++ htdocs/app/sections/partner.blade.php
@@ -1,12 +1,10 @@
 <?php
 	$partners = $block->getPartners();
-	$title = $block->getTitle();
 ?>
 <div class="section-partner">
 	<div class="section__content">
 		<div class="partner section--margin">
-			<div class="partner__title h1">{{$title}}
-			</div>
+			<div class="partner__title h1">Tech<span class="partner__break-one">- </span>nologie<span class="partner__break-two">- </span>partner</div>
 			@foreach ($partners as $partner)
 				<?php
 					$img = $partner['image'];
