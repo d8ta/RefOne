beb3ca0e0f0b48a58dcc3665ccec49e7de98a6ce
Daniel Raudschus <daniel.raudschus@a365.at>
2017-08-17 10:23:50 +0200

    slider section ready (WIP: responsive slider section)

--- htdocs/app/sections/slider.blade.php
+++ htdocs/app/sections/slider.blade.php
@@ -11,8 +11,8 @@
 		<div class="slider__slide">
 			<div class="slider__slide__image bg-image" style="background-image: url({{$slide['background']}})">
 			</div>
-			<div class="slider__slide__headline h1">{{$headline}}</div>
-			<div class="slider__slide__text text-base">{{$text}}</div>
+			<div class="slider__slide__headline h4">{{$headline}}</div>
+			<div class="slider__slide__text text-box">{{$text}}</div>
 		</div>
 		@endforeach
 	</div>
