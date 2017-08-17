d3be317ec762db012f8bd1a2dc25300821a5d511
Daniel Raudschus <daniel.raudschus@a365.at>
2017-08-17 15:00:08 +0200

    non responsive footer

---
+++ htdocs/app/views/components/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="section site-footer">
+	<div class="section__content">
+		<div class="site-footer__table">
+			<div class="site-footer__table__col site-footer__table__col--address">
+			</div>
+			<div class="site-footer__table">
+				<div class="site-footer__copy">
+					&copy; {{date("Y")}} {{get_bloginfo()}}
+				</div>
+				@include('components.footer.navigation')
+				<img class="site-footer__logo" src="assets/images/layout/paymentlogo.png" alt="Payment Logo">
+			</div>
+	
+
+		</div>
+	</div>
+</footer>
