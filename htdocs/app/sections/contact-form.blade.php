020d5fe0babed9d24e383f50d7c0bbc236c65e59
Daniel Raudschus <daniel.raudschus@a365.at>
2017-08-21 13:49:47 +0200

    changes from Feedback Daniel L.

--- htdocs/app/sections/contact-form.blade.php
+++ htdocs/app/sections/contact-form.blade.php
@@ -49,7 +49,7 @@ $formHelper = Project\Helpers\FormHelper::getInstance();
                     <?php
                     echo $formHelper->getFormField("company", __('Company'));
                     echo $formHelper->getFormField("firstname", __('Name*'));
-                    echo $formHelper->getFormField("email", __('E-Mailadresse*'), ["type" => "email"]);
+                    echo $formHelper->getFormField("email", __('E-Mail*'), ["type" => "email"]);
                     echo $formHelper->getFormField("phone", __('Phone'));
                     echo $formHelper->getFormField("message", __('Message'), ["type" => "textarea", "size" => "full"]);
                     echo $formHelper->getHoneyField();
